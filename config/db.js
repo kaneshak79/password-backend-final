321ad3bef9c03ea4a1008016d391fcce2e81685b
kaneshak79 <kaneshak79@gmail.com>
2026-02-23 14:15:51 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.BREVO_MONGODB);
+    if (!process.env.BREVO_MONGODB) {
+      throw new Error("BREVO_MONGODB is not defined in environment");
+    }
+
+    await mongoose.connect(process.env.BREVO_MONGODB)
     console.log("MongoDB Connected");
-  } catch (err) {
-    console.error("DB Error", err.message);
+  } catch (error) {
+    console.error("MongoDB Error:", error);
     process.exit(1);
   }
 };

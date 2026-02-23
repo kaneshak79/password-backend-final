2e3cc0355efaddbebc9a6081a8987a375688c674
kaneshak79 <kaneshak79@gmail.com>
2026-02-23 12:07:30 +0530

    first commit

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,13 +1,24 @@
 import nodemailer from "nodemailer";
 
 export const sendResetMail = async (email, link) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,             // use 587 instead of 465
+  secure: false,         // true for 465, false for 587
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, // App Password
+  },
+});
+
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS
+//     }
+//   });
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
